5ea3dbb89dff3a4e336116c70a913f8b9d7f303c
DARogers <davidarogers@protonmail.com>
2021-07-29 01:25:05 -0700

    added time complexity test

--- SCRIPTS/complexity_test.sh
+++ SCRIPTS/complexity_test.sh
@@ -1,4 +1,7 @@
-#!/bin/zsh
+#!/bin/bash
+
+FILE=$5
+echo Testing $FILE
 while getopts 'n:m:' OPTION; do
   case "$OPTION" in
     n)
@@ -10,6 +13,20 @@ while getopts 'n:m:' OPTION; do
       ;;
   esac
 done
-shift "$(($OPTIND -1))"
 
-for i in {1..$mvalue}; do node ./linked-list-timing.js $nvalue; done;
+declare total=0;
+for ((i=1; i<=$mvalue; i++))
+do
+		start=$(date +%s.%N)
+		node $FILE $nvalue
+		dur=$(echo "$(date +%s.%N) - $start" | bc)
+		printf "Test %d: %.6f seconds\n" $i $dur
+		total=$(echo $total + $dur | bc)
+done
+
+declare avg=$(echo $total / $mvalue | bc -l)
+declare linPr=$(echo 2 \* $avg | bc)
+declare qudPr=$(echo 4 \* $avg | bc)
+
+printf "Average execution time: %.6f seconds\n" $avg
+printf "O(n) --> ~%.6f\nO(n^2) --> ~%.6f\n" $linPr $qudPr
